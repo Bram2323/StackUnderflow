b00122f8c5a88b34f9bac6d038cbfd67dd6c502d
Vexarian <julianpasker@gmail.com>
2024-06-03 14:31:08 +0200

    Added router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import { Route, Routes } from "react-router-dom"
 import { useState } from 'react'
 import './App.css'
 import NavBar from './components/shared/NavBar'
@@ -8,6 +9,16 @@ export default function App() {
   return (
     <>
       <NavBar />
+
+      <Routes>
+        <Route path="" element={<p>Home</p>} />
+        <Route path="/vragen" element={<p>Vragen</p>} />
+        <Route path="/leaderbord" element={<p>Leaderbord</p>} />
+        <Route path="/inloggen" element={<p>Inloggen</p>} />
+        <Route path="/registreren" element={<p>Registreren</p>} />
+
+        <Route path="*" element={<p>404 not found</p>} />
+      </Routes>
     </>
   )
 }
