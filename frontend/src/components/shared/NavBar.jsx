05e9241951c12806fb80147c8f5cbf31cc74eed4
Vexarian <julianpasker@gmail.com>
2024-06-03 14:35:56 +0200

    removed home button

--- frontend/src/components/shared/NavBar.jsx
+++ frontend/src/components/shared/NavBar.jsx
@@ -9,7 +9,6 @@ export default function NavBar() {
     return <div className=" sticky top-0 h-14 w-screen bg-white text-gray-900 border-gray-500 border-b ">
         <div className="home-button" onClick={() => navigate("")}><img src={logo} className=" h-14 float-left "></img> <div className=" float-left mt-4 mr-2 ">stack<b>underflow</b></div></div>
         <ul>
-            <li className="nav-button" onClick={() => navigate("")}>Home</li>
             <li className="nav-button" onClick={() => navigate("/vragen")}>Vragen</li>
             <li className="nav-button" onClick={() => navigate("/leaderbord")}>Leaderbord</li>
         </ul>
