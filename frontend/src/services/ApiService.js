d813c67b0e5898472dde7692577d9f1bcf9c02d8
Bram2323 <bram10111213@gmail.com>
2024-06-03 12:19:01 +0200

    Added api service

---
+++ frontend/src/services/ApiService.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:8080/';
+
+export const TOKEN_LOCATION = 'JWT';
+
+
+class ApiService {
+    static get(url){
+        return axios.get(url, this.#getConfig());
+    }
+
+    static post(url, data){
+        return axios.post(url, data, this.#getConfig());
+    }
+
+    static patch(url, data){
+        return axios.patch(url, data, this.#getConfig());
+    }
+
+    static put(url, data){
+        return axios.put(url, data, this.#getConfig());
+    }
+
+    static delete(url){
+        return axios.delete(url, this.#getConfig());
+    }
+
+
+    static #getConfig(){
+        return {
+            baseURL: API_URL,
+            headers: this.#getHeaders()
+        }
+    }
+
+    static #getHeaders() {
+        let token = sessionStorage.getItem(TOKEN_LOCATION)
+        
+        if (token == null) return {};
+        return {Authorization: "Bearer " + token};
+    }
+}
+
+export default ApiService;
