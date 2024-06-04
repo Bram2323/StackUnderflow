dadb6729c165967ee8a17d86b22deea2ace83fbe
Vexarian <julianpasker@gmail.com>
2024-06-04 14:32:53 +0200

    Question are displayed and can be navigated to

--- frontend/src/components/question-overview/QuestionOverview.jsx
+++ frontend/src/components/question-overview/QuestionOverview.jsx
@@ -1,15 +1,41 @@
 import React, { useEffect, useState } from "react";
 import ApiService from "../../services/ApiService";
+import { useNavigate } from "react-router-dom"
 
-export default function QuestionOverview() {
+function QuestionOverview() {
     const [questions, setQuestions] = useState();
-    //const { id } = useParams();
+
+    const navigate = useNavigate();
 
     useEffect(() => {
         ApiService.get("vragen").then((response) =>
             setQuestions(response.data)
+            //console.log(response.data[0].title)
         );
     }, []);
 
-    return <div>Test</div>
+    if (questions === undefined) {
+        return <></>;
+    }
+
+    // for (let i = 0; i < questions.length; i++) {
+    //     console.log(questions[i].title);
+    //     const creationDate = new Date(questions[i].date);
+    //     const formattedDate = new Intl.DateTimeFormat("nl-NL", {
+    //         dateStyle: "short",
+    //         timeStyle: "short",
+    //         timeZone: "Europe/Amsterdam",
+    //     }).format(creationDate);
+    // }
+
+    // const listItems = questions.map((question) =>
+    //     <div>{question.title} {question.user.username} {question.date}</div>
+    // );
+
+    return <div>
+        {questions.map((question) =>
+            <div key={question.id} onClick={() => navigate(`/vragen/${question.id}`)}>title: {question.title} username: {question.user.username} date: {question.date}</div>)}
+    </div>;
 }
+
+export default QuestionOverview;
