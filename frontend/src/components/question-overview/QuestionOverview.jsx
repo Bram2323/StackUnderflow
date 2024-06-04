875e06aff3dbe1702933a58bf0b7b4c4f7d76dd3
Vexarian <julianpasker@gmail.com>
2024-06-04 15:09:41 +0200

    added some CSS

--- frontend/src/components/question-overview/QuestionOverview.jsx
+++ frontend/src/components/question-overview/QuestionOverview.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import ApiService from "../../services/ApiService";
 import { useNavigate } from "react-router-dom"
+import "./QuestionOverview.css"
 
 function formatDate(date) {
     const creationDate = new Date(date);
@@ -31,7 +32,13 @@ function QuestionOverview() {
 
     return <div>
         {questions.map((question) =>
-            <div key={question.id} onClick={() => navigate(`/vragen/${question.id}`)}>title: {question.title} username: {question.user.username} date: {formatDate(question.date)}</div>)}
+            <div className="flex justify-center">
+                <div className=" bg-gray-300 pt-2 pb-2 pl-2 mt-2 ml-2 w-1/2 " key={question.id}>
+                    <div className="question-title" onClick={() => navigate(`/vragen/${question.id}`)}>{question.title}</div>
+                    <div>geplaatst door: {question.user.username}</div>
+                    <div>datum: {formatDate(question.date)}</div>
+                </div>
+            </div>)}
     </div>;
 }
 
