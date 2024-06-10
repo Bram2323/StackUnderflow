18f46999f081d7cc696204a7700f82e18bcb6257
simonekouters <140211850+simonekouters@users.noreply.github.com>
2024-06-10 20:50:08 +0200

    Show different homepage when not logged in

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import QuestionList from "../shared/question-list/QuestionList.jsx";
+import QuestionList from "../shared/question-list/QuestionList";
 import ApiService from "../../services/ApiService";
 import { useEffect } from "react";
 import UserService from "../../services/UserService";
@@ -21,9 +21,11 @@ function Home() {
     return (
         <>
             {UserService.isLoggedIn() ? (
-                <div className="flex flex-col w-3/4">
+                <div className="flex flex-col w-3/5">
                     <div className="flex items-baseline justify-between gap-10 mt-12">
-                        <h1 className="font-bold text-center ">Mijn vragen</h1>
+                        <h1 className="font-bold text-center text-2xl">
+                            Mijn vragen
+                        </h1>
                         <button
                             className="bg-blue-500 text-white rounded-full px-5 py-3 transition duration-200 my-2 hover:bg-blue-700"
                             onClick={() => navigate("/vragen/aanmaken")}
@@ -34,7 +36,22 @@ function Home() {
                     <QuestionList questions={questions} />
                 </div>
             ) : (
-                <h1>Welkom bij Stack Underflow</h1>
+                <div className="w-1/2 flex flex-col justify-center items-center text-center gap-12 mt-28 px-10 py-16 border-2 border-slate-300 rounded-lg bg-slate-100">
+                    <h1 className="text-4xl leading-normal font-bold">
+                        Elke <span className="text-[#12A8C0]">ITvitae'er</span>{" "}
+                        heeft een tabblad openstaan met Stack Underflow
+                    </h1>
+                    <h2 className="text-lg">
+                        Vind het beste antwoord op jouw technische vraag, help
+                        anderen met het beantwoorden van die van hen.
+                    </h2>
+                    <button
+                        className="register-button"
+                        onClick={() => navigate("/registreren")}
+                    >
+                        Registreer je nu!
+                    </button>
+                </div>
             )}
         </>
     );
