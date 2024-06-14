72a4bbbfae1064eab44948b76ac0de6f02dd3b8f
Vexarian <julianpasker@gmail.com>
2024-06-14 11:04:20 +0200

    frontend van now display ranks

--- backend/src/main/java/com/itvitae/stackunderflow/leaderboard/LeaderboardController.java
+++ backend/src/main/java/com/itvitae/stackunderflow/leaderboard/LeaderboardController.java
@@ -4,10 +4,9 @@ import com.itvitae.stackunderflow.user.User;
 import com.itvitae.stackunderflow.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("leaderboard")
@@ -25,6 +24,9 @@ public class LeaderboardController {
             userRepository.save(user);
         }
 
+        // ISSUE: if the previous users in the leaderboard has the same amount of points they don't share a rank
+        // May have to implement a check to see the points of the previous user on the leaderboard and assign the same
+        // rank if the points are equal.
         Integer rank = 1;
         for (User user : userRepository.findAllByOrderByTotalPointsDesc()) {
             user.setLeaderboardRanking(rank);
@@ -32,5 +34,10 @@ public class LeaderboardController {
             rank++;
         }
         return ResponseEntity.ok("Successfully updated the leaderboard ranking of all users!");
+    }
+
+    @GetMapping("get-all-users")
+    public List<User> getAllUsers() {
+        return userRepository.findAllByOrderByTotalPointsDesc();
     }
 }
--- frontend/src/components/leaderboard/Leaderboard.jsx
+++ frontend/src/components/leaderboard/Leaderboard.jsx
@@ -1,10 +1,24 @@
+import React, { useEffect, useState } from "react";
+import ApiService from "../../services/ApiService";
 import Pageable from "../shared/pageable/Pageable";
+import LeaderboardItem from "./LeaderboardItem";
 
-export default function Leaderboard({ users, totalPages }) {
+export default function Leaderboard({ totalPages }) {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        const url = "leaderboard/get-all-users";
+        ApiService.get(url).then((response) => {
+            setUsers(response.data);
+        });
+    }, []);
+
     return (
         <>
             <Pageable totalPages={totalPages} />
-            INSERT CONTENT HERE
+            {users.map((users, index) => (
+                <LeaderboardItem user={users} key={index} />
+            ))}
             <Pageable totalPages={totalPages} />
         </>
     );
