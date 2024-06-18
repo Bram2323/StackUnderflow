70bd3c2169a6f5dff44b733ca7159ac7899800db
Vexarian <julianpasker@gmail.com>
2024-06-18 10:25:15 +0200

    changed long to primitive type

--- backend/src/main/java/com/itvitae/stackunderflow/user/UserDTO.java
+++ backend/src/main/java/com/itvitae/stackunderflow/user/UserDTO.java
@@ -2,7 +2,7 @@ package com.itvitae.stackunderflow.user;
 
 import java.util.UUID;
 
-public record UserDTO(String username, String award, long totalPoints, long leaderboardRanking, UUID id) {
+public record UserDTO(String username, String award, Long totalPoints, Long leaderboardRanking, UUID id) {
     public static UserDTO from(User user) {
         return new UserDTO(user.getUsername(), user.getAward().toString(), user.getLastMonthPoints(), user.getLastMonthRank(), user.getId());
     }
